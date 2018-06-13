4e595630a39031d1214c9a5b6170aa1999577076
SpeedProg <speedprogde@googlemail.com>
2018-06-13 23:27:52 +0200

    fixed not properly check if a character has a owner when updating association from auth

--- managers/standings.py
+++ managers/standings.py
@@ -197,8 +197,12 @@ class StandingsManager:
         for c in chars:
             logger.debug("Updating Association from Auth for %s", c.character_name)
             try:
-                ownership = CharacterOwnership.objects.get(character=c)
-                main = ownership.user.profile.main_character.character_id if ownership.user.profile.main_character else None
+                try:
+                    ownership = CharacterOwnership.objects.get(character=c)
+                    main = ownership.user.profile.main_character.character_id if ownership.user.profile.main_character else None
+                except CharacterOwnership.DoesNotExist:
+                    main = None
+
                 assoc, created = CharacterAssociation.objects.update_or_create(character_id=c.character_id,
                                                                                defaults={'corporation_id': c.corporation_id,
                                                                                          'main_character_id': main,
