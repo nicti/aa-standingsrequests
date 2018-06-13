bb44564c3c00d22889d9a37eb35f88578f76a15c
SpeedProg <speedprogde@googlemail.com>
2018-06-13 18:34:59 +0200

    removed accidentally commited comments those where in there because my local caching did not work at all anymore. Turns out django redis cache uses redis for locking(without a timeout) so if the python runtime ever shutsdown after locking and before reaching the finally it is never able to lock again.

--- views.py
+++ views.py
@@ -274,9 +274,6 @@ def view_pilots_standings_json(request):
 
     # Cache result for 10 minutes, with a large number of standings this view can be very CPU intensive
     pilots = cache.get_or_set('standings_requests_view_pilots_standings_json', get_pilots, timeout=60*10)
-#    if pilots is None:
-#        pilots = get_pilots()
-#        cache.add('standings_requests_view_pilots_standings_json', pilots)
 
     return JsonResponse(pilots, safe=False)
 
